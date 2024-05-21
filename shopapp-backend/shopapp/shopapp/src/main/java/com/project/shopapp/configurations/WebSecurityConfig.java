5943fb77cd484994db0c5389b9a992f4c45d88b7
Hoàng Nguyên <110187720+hoangnguyenhtng@users.noreply.github.com>
2024-05-21 20:13:40 +0700

    Add: Feature Filter Chain

--- shopapp-backend/shopapp/shopapp/src/main/java/com/project/shopapp/configurations/WebSecurityConfig.java
+++ shopapp-backend/shopapp/shopapp/src/main/java/com/project/shopapp/configurations/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.project.shopapp.configurations;
 
+import com.project.shopapp.filters.JwtTokenFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,15 +8,17 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableMethodSecurity
 @RequiredArgsConstructor
 public class WebSecurityConfig {
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity, JwtTokenFilter jwtTokenFilter) throws Exception{
         httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
+                .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class)
         .authorizeHttpRequests(requests -> {
             requests.requestMatchers("**")
                     .permitAll();
